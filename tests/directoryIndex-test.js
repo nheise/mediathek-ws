22137f78fa382f5aee2eddb527e3b047387f120a
Niklas Heise <niklas.heise@quirion.de>
2018-10-26 20:10:03 +0200

    check entry name

--- tests/directoryIndex-test.js
+++ tests/directoryIndex-test.js
@@ -16,11 +16,13 @@ o.spec('directoryIndex', function () {
       if( directoryIndex.size() == 7 ) {
 
         const dir1Entry = directoryIndex.getIndexEntryForPath( 'dir1' )
+        o( dir1Entry.name ).equals( 'dir1' )
         o( dir1Entry.type ).equals( directoryIndex.entryType.DIRECTORY )
         o( dir1Entry.subEntrys.length ).equals( 2 )
         o( dir1Entry.parent.name ).equals( '' )
         
         const file11Entry = directoryIndex.getIndexEntryForPath( 'dir1/dir11/file11' )
+        o( file11Entry.name ).equals( 'file11' )
         o( file11Entry.type ).equals( directoryIndex.entryType.FILE )
         o( file11Entry.subEntrys.length ).equals( 0 )
         o( file11Entry.size ).equals( 0 )

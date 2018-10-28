7334d10539eee53dfd93a8260a27a4a5cca49231
Niklas Heise <niklas.heise@quirion.de>
2018-10-28 20:29:16 +0100

    some modifications

--- webservice.js
+++ webservice.js
@@ -8,29 +8,30 @@ app.get('/directories/:key', function (req, res) {
 
     const indexEntry = directoryIndex.getIndexEntryForPath( req.params.key )
     if( indexEntry != undefined ) {
-        res.json( mapToResource( 0 )( indexEntry ) )
+        res.json( mapResourceWithDepthCheck( 1 )( indexEntry ) )
     }
     else {
         res.status(404).json( { error: { code: 404, text: `Directory ${req.params.key} not found.`} } )
     }
 })
 
-function mapToResource( depth = 0 ) {
-  if( depth > 1 ) {
-    return () => {}
-  } else {
+function mapResourceWithDepthCheck( maxDepth,  depth = 0 ) {
+  if( depth < maxDepth ) {
+    const goDown = depth + 1
     return ( indexEntry ) => {
       return {
         name: indexEntry.name,
         path: indexEntry.path,
         type: indexEntry.type,
-        subEntries: indexEntry.subEntries.map( mapToResource( ++depth ) ),
+        subEntries: indexEntry.subEntries.map( mapResourceWithDepthCheck( maxDepth, goDown ) ),
         links: { 
           self: `http://localhost:3000/directories/${indexEntry.path}`,
           parent: `http://localhost:3000/directories/${indexEntry.parent.path}`
         }
       }
     }
+  } else {
+    return () => {}
   }
 }
 
